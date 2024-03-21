3498824b8dddc311cf751784109f654d53477c2f
Abraham <abukip344@gmail.com>
2024-03-20 23:53:29 -0400

    functions as values

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callMe) {
+    callMe();
+}
+
+function returnsANamedFunction(){
+    return function greeting (){
+        
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+       
+    }
+}
+
